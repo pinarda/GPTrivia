2f0254bed4f029e8c3f7a2bf1c6a1b7bf6ee6966
Alex <pinard.alex@gmail.com>
2023-04-02 23:53:48 -0600

    python path

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,10 @@
+import sys
+
 workers = 3
 bind = "0.0.0.0:8000"
 
 module = "GPTrivia.wsgi:application"
 
 raw_env = ["DJANGO_SETTINGS_MODULE=GPtrivia.settings"]
+
+sys.path.insert(0, "/Users/rawrhouse/git/GPTrivia")
