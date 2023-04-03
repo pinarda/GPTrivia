61c3d56fab0a244037c84ac2d42384b8b879cf8a
Alex <pinard.alex@gmail.com>
2023-04-02 23:50:43 -0600

    gunicorn stuff

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,6 @@
-import os
-
 workers = 3
 bind = "0.0.0.0:8000"
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GPTrivia.settings")
-
 module = "GPTrivia.wsgi:application"
+
+raw_env = ["DJANGO_SETTINGS_MODULE=GPtrivia.settings"]
