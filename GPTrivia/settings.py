535e5d389c39aad3e74b17dcc03cc4cae4312be8
Alex <pinard.alex@gmail.com>
2023-04-06 11:52:08 -0600

    add stuff

--- GPTrivia/settings.py
+++ GPTrivia/settings.py
@@ -49,10 +49,14 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticated'
+
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
     ],
 }
 
