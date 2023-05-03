c56fa7e9fbb37ad959b2b35eeca4e03d57d25544
Alex <pinard.alex@gmail.com>
2023-05-02 18:51:29 -0600

    fix

--- manage.py
+++ manage.py
@@ -9,9 +9,12 @@ def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GPTrivia.settings")
     try:
         from django.core.management import execute_from_command_line
-    except:
-        pass
-
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
     execute_from_command_line(sys.argv)
 
 
