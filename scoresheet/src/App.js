11a621d538f3126fb85ad62f9cee45983b90d5dc
Alex <pinard.alex@gmail.com>
2023-10-06 16:14:35 -0600

    try passing csrf token

--- scoresheet/src/App.js
+++ scoresheet/src/App.js
@@ -388,11 +388,30 @@ const PlayerTable = () => {
        console.log("isSaved updated:", isSaved);
     }, [isSaved]);
 
+    function getCookie(name) {
+    let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            let cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                let cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
+    let csrfToken = getCookie('csrftoken');
+
     useEffect(() => {
         fetch(url + '/api-token-auth/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
+                'X-CSRFToken': csrfToken   // Add the CSRF token here
             },
             body: JSON.stringify({
                 username: "Alex",
