4d2f3bf93b7212b79fe0cc712a4cc0a049c04a6e
Alex <pinard.alex@gmail.com>
2023-11-23 16:34:28 -0700

    d

--- GPTrivia/settings.py
+++ GPTrivia/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-dnu^#ycvr7cm#x900=e2@whtrf7xj7#6--528a6tmm)-$5x$1k"
 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
