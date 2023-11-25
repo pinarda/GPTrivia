3fba8b36e49b7471e40f1871c5817a8fc53b4984
Alex <pinard.alex@gmail.com>
2023-11-24 17:44:40 -0700

    d

--- GPTrivia/serializers.py
+++ GPTrivia/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from GPTrivia.models import GPTriviaRound, MergedPresentation
+import json
 
 class GPTriviaRoundSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +11,10 @@ class MergedPresentationSerializer(serializers.ModelSerializer):
     class Meta:
         model = MergedPresentation
         fields = '__all__' # Or specify the fields you want to expose in the API
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        # Serialize round_names and creator_list as JSON if they are not None
+        if ret['joker_round_indices'] is not None:
+            ret['joker_round_indices'] = json.loads(ret['joker_round_indices'])
+        return ret
