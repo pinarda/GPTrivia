b235545c5b55c1b53845f64d14d75033749b97bd
Alex <pinard.alex@gmail.com>
2023-12-13 21:15:12 -0700

    d

--- scoresheet/src/App.js
+++ scoresheet/src/App.js
@@ -795,7 +795,6 @@ const PlayerTable = () => {
                 [roundTitle]: newScore,
             },
         });
-        saveData();
     };
 
 
@@ -1081,15 +1080,8 @@ const PlayerTable = () => {
           console.error('Error:', error);
         });
 
-        if (wsRef.current && wsRef.current.readyState === WebSocket.OPEN) {
-            console.log('Sending update message to WebSocket via React');
-            isLocalUpdate.current = true;
-            console.log ('isLocalUpdate set to true at ', new Date().toLocaleTimeString());
-            wsRef.current.send(JSON.stringify({type: 'scoresheet_message', message: {'action': 'update'}}));
-            setTimeout(() => isLocalUpdate.current = false, 10000); // Reset flag after a short delay
-        } else {
-            console.log('WebSocket is not open. Current state:', wsRef.current.readyState);
-        }
+
+
 
         setIsSaved(true);
     };
@@ -1143,6 +1135,16 @@ const PlayerTable = () => {
         .catch((error) => {
             console.error('Error:', error);
         });
+
+        if (wsRef.current && wsRef.current.readyState === WebSocket.OPEN) {
+            console.log('Sending update message to WebSocket via React');
+            isLocalUpdate.current = true;
+            console.log ('isLocalUpdate set to true at ', new Date().toLocaleTimeString());
+            wsRef.current.send(JSON.stringify({type: 'scoresheet_message', message: {'action': 'update'}}));
+            setTimeout(() => isLocalUpdate.current = false, 10000); // Reset flag after a short delay
+        } else {
+            console.log('WebSocket is not open. Current state:', wsRef.current.readyState);
+        }
     }
 
     const handleTempTitleChange = (index, newTitle) => {
@@ -1181,6 +1183,16 @@ const PlayerTable = () => {
         .catch((error) => {
           console.error('Error:', error);
         });
+
+        if (wsRef.current && wsRef.current.readyState === WebSocket.OPEN) {
+            console.log('Sending update message to WebSocket via React');
+            isLocalUpdate.current = true;
+            console.log ('isLocalUpdate set to true at ', new Date().toLocaleTimeString());
+            wsRef.current.send(JSON.stringify({type: 'scoresheet_message', message: {'action': 'update'}}));
+            setTimeout(() => isLocalUpdate.current = false, 10000); // Reset flag after a short delay
+        } else {
+            console.log('WebSocket is not open. Current state:', wsRef.current.readyState);
+        }
     };
 
 
