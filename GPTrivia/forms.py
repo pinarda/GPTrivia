4ef21e13d8c0c8a0231c5e00544c27bb47f04924
Alex <pinard.alex@gmail.com>
2024-01-16 18:27:14 -0700

    tst

--- GPTrivia/forms.py
+++ GPTrivia/forms.py
@@ -8,7 +8,7 @@ class GPTriviaRoundForm(forms.ModelForm):
             'creator', 'title', 'major_category', 'minor_category1', 'minor_category2',
             'date', 'round_number', 'max_score',
             'score_alex', 'score_ichigo', 'score_megan', 'score_zach', 'score_jenny', 'score_debi',
-            'score_dan', 'score_chris', 'score_drew'
+            'score_dan', 'score_chris', 'score_drew', 'score_tom'
         ]
 
 class ProfilePictureForm(forms.ModelForm):
