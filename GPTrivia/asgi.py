3b745dec12c40216bd245622a197746b8476ba42
Alex <pinard.alex@gmail.com>
2024-07-03 16:43:18 -0600

    d

--- GPTrivia/asgi.py
+++ GPTrivia/asgi.py
@@ -1,14 +1,3 @@
-"""
-ASGI config for GPTrivia project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
-"""
-
-# asgi.py in your Django project's main directory
-
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -18,11 +7,8 @@ from GPTrivia.routing import websocket_urlpatterns
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GPTrivia.settings')
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": get_asgi_application(),  # Handles HTTP requests
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
+        URLRouter(websocket_urlpatterns)
     ),
 })
-
--- GPTrivia/routing.py
+++ GPTrivia/routing.py
@@ -1,19 +1,6 @@
 from django.urls import path
 from .consumers import ScoresheetConsumer
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-import os
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GPTrivia.settings')
 
 websocket_urlpatterns = [
     path('ws/scoresheet/', ScoresheetConsumer.as_asgi()),
 ]
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),  # Handles HTTP requests
-    "websocket": AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
-    ),
-})
