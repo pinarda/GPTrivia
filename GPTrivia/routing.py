14955be78a63ff0cd4cd42adf6cfa0df3b82735d
Alex <pinard.alex@gmail.com>
2024-07-03 16:30:43 -0600

    d

--- GPTrivia/routing.py
+++ GPTrivia/routing.py
@@ -1,13 +1,16 @@
-# routing.py in your Django app
-
 from django.urls import path
 from .consumers import ScoresheetConsumer
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
 
 websocket_urlpatterns = [
     path('ws/scoresheet/', ScoresheetConsumer.as_asgi()),
 ]
 
 application = ProtocolTypeRouter({
-    'websocket': URLRouter(websocket_urlpatterns),
+    "http": get_asgi_application(),  # Handles HTTP requests
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
 })
