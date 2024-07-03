3ec06a225a2b9289678853f8e3db5cdd47dbb4be
Alex <pinard.alex@gmail.com>
2024-07-03 16:34:46 -0600

    d

--- GPTrivia/routing.py
+++ GPTrivia/routing.py
@@ -3,6 +3,9 @@ from .consumers import ScoresheetConsumer
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GPTrivia.settings')
 
 websocket_urlpatterns = [
     path('ws/scoresheet/', ScoresheetConsumer.as_asgi()),
