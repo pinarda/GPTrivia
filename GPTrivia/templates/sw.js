1780c8553ebcfd5adc10ebe14910399ce8a4aa82
Alex <pinard.alex@gmail.com>
2026-03-20 10:48:53 -0600

    Stop service worker from serving stale scoresheet assets

--- GPTrivia/templates/sw.js
+++ GPTrivia/templates/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "hail-science-trivia";
+const CACHE_NAME = "hail-science-trivia-v2";
 // const STATIC_ASSETS = [
 //   "/",
 //   "/static/"
@@ -18,16 +18,18 @@ self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   );
   console.log('SW: Activated');
 });
 
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(
-      cached => cached || fetch(event.request)
-    )
+    fetch(event.request).catch(() => caches.match(event.request))
   );
   console.log('SW: Fetched');
 });
